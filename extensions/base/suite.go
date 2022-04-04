058ccc51c07ae4e5d1ac30513b0cd3b9a3503b40
Artem Glazychev <artem.glazychev@xored.com>
2022-04-04 15:09:43 +0700

    Ping pong
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>

--- extensions/base/suite.go
+++ extensions/base/suite.go
@@ -52,7 +52,7 @@ func (s *Suite) TearDownSuite() {
 }
 
 const (
-	sha = "97f3b622c1de62a6693443fbc872bcc57c238052"
+	sha = "6191c2aa01bcd9883e5d0b4f93810ca65d935cb7"
 )
 
 // SetupSuite runs all extensions
